d40c419ec80d14b86b4e6e3850e0baf337a984bb
tony <zhujf620@gmail.com>
2016-12-08 20:27:54 +0800

    升级为0.2版本，根据文件目录结构解析生成对应i18n内容的JSON

--- index.js
+++ index.js
@@ -10,8 +10,22 @@
 'use strict';
 
 var fs = require('fs');
+var path = require('path');
+var walk = require('walk');
 var ejs = require('ejs');
 var extend = require('extend');
+
+function propDeepVerifier (obj, propsPath, i, content) {
+  if (obj[propsPath[i]] === undefined) {
+    obj[propsPath[i]] = {}
+  }
+  var next = i + 1
+  if (next < propsPath.length) {
+    propDeepVerifier(obj[propsPath[i]], propsPath, next, content)
+  } else if (next === propsPath.length) {
+    obj[propsPath[i]] = content
+  }
+}
 
 module.exports = function(srcOptions, modified, total, next) {
   var options = {
@@ -25,77 +39,89 @@ module.exports = function(srcOptions, modified, total, next) {
   extend(options,srcOptions);
 
   //read i18n language json file
-  var langList = [],
+  var langList = {},
     langDefault = null;
-  fs.readdirSync(fis.project.getProjectPath()+'/'+options.i18n).map(function(f){
-    var langPrefix = /^(.*[^\s])\.json$/.exec(f);
-    if(langPrefix && langPrefix[1]){
-      langPrefix = langPrefix[1];
-      try{
-        langList[langPrefix] = JSON.parse(fs.readFileSync(fis.project.getProjectPath()+'/'+options.i18n+'/'+f));
-        if ((options.default+'') === langPrefix) {
-          langDefault = langPrefix;
-        }
-      }catch(e){
-        console.error('\nparse json failed:'+ fis.project.getProjectPath()+'/'+options.i18n+'/'+f);
-        throw e;
-      }
+
+  var i18nResourcePath = path.join(fis.project.getProjectPath(), options.i18n)
+
+  var walker = walk.walk(i18nResourcePath, { followLinks: false })
+
+  walker.on('file', (root, fileStat, next) => {
+    var propsPath = root.replace(i18nResourcePath, '').replace(/^\//, '').replace(/\/$/, '').split('/')
+    propsPath.push(fileStat.name.replace(/\.(json|js)$/, ''))
+    propsPath = propsPath.filter(function (propPath) {
+      return propPath && propPath.length !== 0
+    })
+    var filePath = path.join(root, fileStat.name)
+    var content = null
+    if (/\.js$/.test(fileStat.name)) {
+      content = require(filePath)
+    } else if (/\.json$/.test(fileStat.name)) {
+      content = JSON.parse(fs.readFileSync(filePath, 'utf8'))
     }
+    propDeepVerifier(langList, propsPath, 0, content)
+    next()
   });
 
-  //generate html file from i18n file and template file
-  var distHtmlFiles = [];
-  modified.forEach(function(file) {
-    if(file.isHtmlLike){
-      if (!/^_/.test(file.filename)) {
-        var template = ejs.compile(file.getContent(),{'open':options.open,'close':options.close});
+  walker.on("errors", (root, nodeStatsArray, next) => {
+    console.error('\nparse json failed:'+ root);
+  });
 
-        for(var lang in langList){
-          var path = /([^\/]+)(?=\.html)/.exec(file.subpath);
-          var distfile = options.dist.replace('$lang',((langDefault!==null && ~lang.indexOf(langDefault))?'':lang));
-          var fileReplace = path[0];
-          if (options.keepParentDirectoryList && options.keepParentDirectoryList.length !== 0) {
-            var needKeep = false,
-              needKeepDirectoryName = '',
-              fileSubpathSplit = file.subpath.split('/');
+  walker.on("end", () => {
+    //generate html file from i18n file and template file
+    var distHtmlFiles = [];
+    modified.forEach(function(file) {
+      if(file.isHtmlLike){
+        if (!/^_/.test(file.filename)) {
+          var template = ejs.compile(file.getContent(),{'open':options.open,'close':options.close});
 
-            options.keepParentDirectoryList.forEach(function (dirKeyword) {
-              // 多层目录的判断
-              if (~dirKeyword.indexOf('/')){
-                if (~file.subpath.indexOf(dirKeyword.replace(/\/$/, '')+'/')){
-                  needKeep = true;
-                  needKeepDirectoryName = dirKeyword;
-                  return;
+          for(var lang in langList){
+            var path = /([^\/]+)(?=\.html)/.exec(file.subpath);
+            var distfile = options.dist.replace('$lang',((langDefault!==null && ~lang.indexOf(langDefault))?'':lang));
+            var fileReplace = path[0];
+            if (options.keepParentDirectoryList && options.keepParentDirectoryList.length !== 0) {
+              var needKeep = false,
+                needKeepDirectoryName = '',
+                fileSubpathSplit = file.subpath.split('/');
+
+              options.keepParentDirectoryList.forEach(function (dirKeyword) {
+                // 多层目录的判断
+                if (~dirKeyword.indexOf('/')){
+                  if (~file.subpath.indexOf(dirKeyword.replace(/\/$/, '')+'/')){
+                    needKeep = true;
+                    needKeepDirectoryName = dirKeyword;
+                    return;
+                  }
+                } else {
+                  if (~fileSubpathSplit.indexOf(dirKeyword)) {
+                    needKeep = true;
+                    needKeepDirectoryName = dirKeyword;
+                    return;
+                  }
                 }
-              } else {
-                if (~fileSubpathSplit.indexOf(dirKeyword)) {
-                  needKeep = true;
-                  needKeepDirectoryName = dirKeyword;
-                  return;
-                }
+              })
+
+              if (needKeep === true) {
+                fileReplace = needKeepDirectoryName + '/' + path[0];
               }
-            })
-
-            if (needKeep === true) {
-              fileReplace = needKeepDirectoryName + '/' + path[0];
             }
+
+            distfile = distfile.replace('$file', fileReplace);
+
+            var htmlFile = fis.file(fis.project.getProjectPath(),'/'+distfile+'.html');
+            htmlFile.setContent(template(langList[lang]));
+            distHtmlFiles.push(htmlFile);
           }
-
-          distfile = distfile.replace('$file', fileReplace);
-
-          var htmlFile = fis.file(fis.project.getProjectPath(),'/'+distfile+'.html');
-          htmlFile.setContent(template(langList[lang]));
-          distHtmlFiles.push(htmlFile);
         }
       }
-    }
-  });
+    });
 
-  //add to deplay file array
-  distHtmlFiles.forEach(function(file){
-    modified.push(file);
-  });
+    //add to deplay file array
+    distHtmlFiles.forEach(function(file){
+      modified.push(file);
+    });
 
-  //invoke the next deploy plugin
-  next();
+    //invoke the next deploy plugin
+    next();
+  });
 };
